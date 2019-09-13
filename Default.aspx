d21e98d52dbe293e3abe22fc9714a8ac458b4ff2
srecckog <srecckog@gmail.com>
2019-09-13 11:22:33 +0200

    first commit

---
+++ Default.aspx
@@ -0,0 +1,93 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>    
+       <link href="DigStyle0.css" rel="stylesheet" type="text/css" /> 
+</head>
+ <body style="background-color:#000000">
+
+    <form id="form1" runat="server">
+
+<asp:ScriptManager ID="ScriptManager1" runat="server">
+
+        </asp:ScriptManager>
+        <asp:Timer ID="TimerTime" runat="server" Interval="1000">
+        </asp:Timer>
+     
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+ 
+            <ContentTemplate>                
+          <link href="DigStyle0.css" rel="stylesheet" type="text/css" /> 
+                <body style="background-color:#000000">
+                <div id="clockdate">
+            <div class="clockdate-wrapper">
+                
+                <div id="date"><asp:Label ID="lblDateToday" runat="server" ></asp:Label></div>
+                <div id="clock"><asp:Label ID="lblTime"  runat="server" CssClass="clock"></asp:Label></div>
+               <%-- <asp:Image ID="Image1" runat="server" src="Img/images2.jpg" alt="slikabozic" height="600"  width="1200" />              
+                <div id="tdole">     
+                                            
+                      <asp:Literal ID="tdolet" runat="server" Text=''/>                       
+                </div>	--%>
+                
+		        <div id="tdole2">
+                    
+    <asp:Panel ID="myPanel" runat="server" >
+        <!-- Other markup here like client and server controls/elements -->
+    
+                <%--<div id="tdole211">--%>
+                           <font size="7"  >
+                          <%-- <br /><br />--%>
+                                   <%--<br /><br />--%>
+                                   <%--<br><br />--%>
+
+                                      <table width="100%" align="center" cellpadding="2" cellspacing="2" border="2"  bgcolor="#004080" >
+        
+                                        <tr align="center" style="background-color:#004080;color:White; font-size: 1.95em;">
+                                           <th align="center" colspan="3" >Realizacija Feroimpex</th>
+                                        </tr>
+                                        <tr align="center" style="background-color:#004080;color:White; font-size: 1.25em;">
+                                            <%--<tr align="center" style="background-color:#0094ff;color:White; font-size: 1.25em;">--%>
+                                             <td text-align: center;  vertical-align: middle; &:last-child { font-size: 0.95em; line-height: 1.4;text-align: left; width="40%"> PERIOD </td>                        
+                                             <td width="30%"> CILJ </td>            
+                                             <td width="30%">OSTVARENO</td> 
+       <%-- <td width="15%">CILJ ZA SMJENU</td> 
+        <td width="15%">SMJENA 1</td> 
+        <td width="15%">SMJENA 2</td> 
+        <td width="15%">SMJENA 3</td> --%>
+                                            </tr>
+                                                     <%=wogrid()%>        
+                                       
+                                          </table>
+                      <%--</div>--%>
+                               </asp:Panel>
+                      <asp:Literal ID="tdole2t" runat="server" Text=''/>             
+		        
+                
+             </div>
+
+
+            </div>
+                    
+
+                
+            </ContentTemplate>
+            
+            <Triggers>
+                <asp:AsyncPostBackTrigger ControlID="TimerTime" EventName="Tick" />
+            </Triggers>
+
+        </asp:UpdatePanel>
+       
+        <br />
+
+    
+    
+       
+    
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Globalization;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        //int i1 = 0; // info pult
+        //i1 = 1; // hala1 pauza
+        //i1 = 2; // hala3 pauza
+        //i1 = 0;
+        int  i1 = (int.Parse)(ConfigurationManager.AppSettings["tipporuke"]);
+        
+        if (i1 == 1)
+        {
+            lblTime.Text = DateTime.Now.ToString("HH:mm:ss");
+            DateTime tv = DateTime.Now;
+            lblDateToday.Text = DateTime.Now.ToString("dddd, dd.MM.yyyy") + "<br>";
+           
+            tdole2t.Text = "";
+            myPanel.Visible = true;
+
+            //Image1.Visible = true;
+            if (((tv.Hour == 9 && tv.Minute > 44) || (tv.Hour == 10 && tv.Minute <= 14)) || (tv.Hour == 18 && tv.Minute <= 30) || (tv.Hour == 2 && tv.Minute <= 30))
+            {
+                //tdole211.Visible = false;
+                myPanel.Visible=false;
+                tdole2t.Text = "P A U Z A";                
+                //Image1.Visible = false;               
+            }
+        }
+        else if (i1==2)
+        {
+
+            lblTime.Text = DateTime.Now.ToString("HH:mm:ss");
+            DateTime tv = DateTime.Now;
+            lblDateToday.Text = DateTime.Now.ToString("dddd, dd.MM.yyyy") + "<br>";
+            tdole2t.Text = "";
+            myPanel.Visible = true;
+            if (((tv.Hour == 10 && tv.Minute <= 30)) || (tv.Hour == 18 && tv.Minute <= 30) || (tv.Hour == 2 && tv.Minute <= 30))
+            {
+                myPanel.Visible = false;
+                tdole2t.Text = "P A U Z A";
+            }
+        }
+
+        else
+        {
+
+
+            // ispis poruke na tv ekran iz rfin.tv_poruka
+           
+
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["MyDbConn"].ToString()))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM tv_poruka where status='1' and tip='1'", cn);
+                cn.Open();
+                SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                rdr.Read();
+                if (rdr.HasRows)
+                    lblDateToday.Text = rdr[0].ToString();
+                //  tdole2t.Text = rdr[0].ToString();
+                //
+                //Response.Write(rdr[0].ToString()); //read a value
+                cn.Close();
+            }
+            
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["MyDbConn"].ToString()))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM tv_poruka where status='1' and tip='2'", cn);
+                cn.Open();
+                SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                rdr.Read();
+                if (rdr.HasRows)
+                    //tdolet.Text = rdr[0].ToString();
+                //Response.Write(rdr[0].ToString()); //read a value
+                cn.Close();
+            }
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["MyDbConn"].ToString()))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM tv_poruka where status='1' and tip='3'", cn);
+                cn.Open();
+                SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                rdr.Read();
+                if (rdr.HasRows)
+                    tdole2t.Text = rdr[0].ToString();
+                //    lblDateToday.Text = rdr[0].ToString();
+
+                //
+                //Response.Write(rdr[0].ToString()); //read a value
+                cn.Close();
+            }
+
+        }
+    }
+
+    public string wogrid()
+    {
+        int kolicinad = 0, kolicinat = 0, kolicinam = 0;
+        string sql1 = "",hala1="1";
+        using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["MyDbConn"].ToString()))
+        {
+            //sql1 = "select * from rfind.dbo.tvinfo_pult where hala='" + hala1 + "'  order by datum desc";
+            sql1 = "select * from rfind.dbo.tvinfo_pult  order by datum desc";
+            SqlCommand cmd = new SqlCommand(sql1, cn);
+            cn.Open();
+
+            SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            string htmlStr = "";
+
+            while (rdr.Read())
+            {
+                kolicinad = rdr.GetInt32(2);
+                kolicinat = rdr.GetInt32(3);
+                kolicinam = rdr.GetInt32(4);
+            }
+            kolicinad = 40000;
+            kolicinat = kolicinad / 3 * 17;
+            kolicinam = kolicinad / 3 * 68;
+
+            cn.Close();
+        }
+
+        using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["MyDbConn"].ToString()))
+        {
+            sql1 = "rfind.dbo.tv_dpr " + hala1 + " ,4";
+            SqlCommand cmd = new SqlCommand(sql1, cn);
+            cn.Open();
+
+            SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            string htmlStr = "";
+            while (rdr.Read())
+            {
+                if (rdr.HasRows)
+                    {
+                    int h = 0;
+                }
+                int kolicina = rdr.GetInt32(3);
+                string tip = rdr.GetString(1);
+                int oznaka = rdr.GetInt32(2);
+                DateTime dat1 = DateTime.Now.AddDays(oznaka);
+                string sdate1 = dat1.Day.ToString() + '-' + dat1.Month.ToString() + '-' + dat1.Year.ToString();
+                sdate1 = dat1.Day.ToString() + '.' + dat1.Month.ToString() + '.' + dat1.Year.ToString();
+
+                DateTimeFormatInfo dfi = DateTimeFormatInfo.CurrentInfo;
+                System.Globalization.Calendar cal = dfi.Calendar;
+                int week1 = cal.GetWeekOfYear(dat1, dfi.CalendarWeekRule, dfi.FirstDayOfWeek);
+                int month1 = dat1.Month;
+
+
+                if (tip == "D")
+                {
+                    htmlStr += "<tr><td>Dan " + sdate1 + "</td><td>" + kolicinad.ToString() + "</td><td>" + kolicina.ToString() + "</td></tr>";
+                }
+                if (tip == "T")
+                {
+                    htmlStr += "<tr><td> Tjedan " + week1.ToString() + "</td><td>" + kolicinat.ToString() + "</td><td>" + kolicina.ToString() + "</td></tr>";
+                }
+                if (tip == "M")
+                {
+                    htmlStr += "<tr><td> Mjesec " + month1.ToString() + "</td><td>" + kolicinam.ToString() + "</td><td>" + kolicina.ToString() + "</td></tr>";
+                }
+
+
+            }
+
+            cn.Close();
+            return htmlStr;
+        }
+
+    }
+    protected void TimerTime_Tick(object sender, EventArgs e)
+    {
+        lblTime.Text = DateTime.Now.ToString("HH:mm:ss");
+        lblDateToday.Text= DateTime.Now.ToString("dd:MM:yyyy");
+
+    }
+}
